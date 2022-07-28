fa07e6d1b79905b0a0176716a3e4d2cfcc82f13b
Tushar Gautam <tushar.gautam.mailbox@gmail.com>
2022-07-28 20:29:34 +0530

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="new ice creams shop" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      This is ice cream
+    </div>
+  )
+}
